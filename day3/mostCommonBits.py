8f5d2dc418e1f0e6f571e12992b2f32eaee172e3
Emil Abraham <emilabraham1414@mail.com>
2021-12-03 17:53:22 -0500

    Cleanup day 3

--- day3/mostCommonBits.py
+++ day3/mostCommonBits.py
@@ -22,6 +22,7 @@ def getMostAndLeastCommonBits(bits):
     elif zeroCount < oneCount:
         return [1,0]
 
+# Get the most common bit. In case of a tie, default to 1
 def getMostCommonBits(bits):
     zeroCount = 0
     oneCount = 0
@@ -36,6 +37,7 @@ def getMostCommonBits(bits):
     elif zeroCount <= oneCount:
         return 1
 
+# Get the least common bit. In case of a tie, default to 0
 def getLeastCommonBits(bits):
     zeroCount = 0
     oneCount = 0
@@ -53,10 +55,18 @@ def getLeastCommonBits(bits):
 # Convert a binary list to a decimal number.
 def binaryToDecimal(binary):
   number = 0
+  for b in binary:
+    number = (2 * number) + b
+  return number
+
+# Convert a binary list to a decimal number while casting as an int and ignoring \n.
+def binaryToDecimalInt(binary):
+  number = 0
   for b in binary[:len(binary)-1]:
     number = (2 * number) + int(b)
   return number
 
+# calculate the gammaRate and epsilonRate
 for i in range(bitLength-1):
     bits = []
     for line in lines:
@@ -65,42 +75,43 @@ for i in range(bitLength-1):
     gammaRate.append(gammaAndEpsilonBits[0])
     epsilonRate.append(gammaAndEpsilonBits[1])
 
-# returns true if the bit is the mostCommonBit
-# Should generalize this for most/least common bits.
-def gammaFilter(line, mostCommonBit, index):
-    if int(line[index]) == mostCommonBit:
+# returns true if the nth value is the bit we are looking for
+def bitFilter(line, bit, index):
+    if int(line[index]) == bit:
         return True
     else:
         return False
 
+# Grabs the nth place of bits from list of lines.
+# Returns as an int list
 def linesToBits(lines, index):
     bits = []
     for line in lines:
         bits.append(int(line[index]))
     return bits
 
-# Loops through lines and filters out based on the gammaRate or the epsilonRate.
+# Loops through lines and filters out based on most common bit
 def filterOxygenGeneratorRating(lines):
     filteredList = lines
     for index in range(bitLength-1):
-        print(filteredList)
         bit = getMostCommonBits(linesToBits(filteredList, index))
         if (len(filteredList) > 1):
-            filteredList = list(filter(lambda line: gammaFilter(line, bit, index), filteredList))
+            filteredList = list(filter(lambda line: bitFilter(line, bit, index), filteredList))
     return filteredList
 
+# Loops through lines and filters out based on least common bit
 def filterCo2ScrubberRating(lines):
     filteredList = lines
     for index in range(bitLength-1):
-        print(filteredList)
         bit = getLeastCommonBits(linesToBits(filteredList, index))
         if (len(filteredList) > 1):
-            filteredList = list(filter(lambda line: gammaFilter(line, bit, index), filteredList))
+            filteredList = list(filter(lambda line: bitFilter(line, bit, index), filteredList))
     return filteredList
 
 
-oxygenGeneratorRating = binaryToDecimal(filterOxygenGeneratorRating(lines)[0])
-co2ScrubberRating = binaryToDecimal(filterCo2ScrubberRating(lines)[0])
+oxygenGeneratorRating = binaryToDecimalInt(filterOxygenGeneratorRating(lines)[0])
+co2ScrubberRating = binaryToDecimalInt(filterCo2ScrubberRating(lines)[0])
+
 print(oxygenGeneratorRating)
 print(co2ScrubberRating)
 print(co2ScrubberRating * oxygenGeneratorRating)
