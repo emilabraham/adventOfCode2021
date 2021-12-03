d94b6b7d21ee4f20387ea3dd44ba1c55be9536b6
Emil Abraham <emilabraham1414@mail.com>
2021-12-02 23:16:23 -0500

    Day 2 part 2

--- day2/moveSub.py
+++ day2/moveSub.py
@@ -3,16 +3,18 @@ fp = open("input")
 lines = fp.readlines()
 depth = 0
 horizontalPosition = 0
+aim = 0
 for line in lines:
     command = line.strip().split(" ")
     direction = command[0]
     distance = int(command[1])
     if direction == "forward":
         horizontalPosition += distance
+        depth += aim * distance
     elif direction == "up":
-        depth -= distance
+        aim -= distance
     elif direction == "down":
-        depth += distance
+        aim += distance
 
 print(depth)
 print(horizontalPosition)
