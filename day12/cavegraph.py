01b24b641f400021f1a1373fe39e13a668fcb07b
Emil Abraham <emilabraham1414@gmail.com>
2021-12-12 12:52:25 -0500

    WIP Day 12 start

---
+++ day12/cavegraph.py
@@ -0,0 +1,8 @@
+# Find all paths through the cave
+# First step is to create a Cave object: Cave(value, isSmall)
+# Second step is to create a dictionary of each cave's connections { Cave.value: list(Cave)}
+# Final step is to create a list of all paths through the cave
+# How do I figure out when I've tried all possible paths?
+# I think recursion might be the way to go?
+fp = open("input2")
+
