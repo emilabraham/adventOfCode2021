eff83ed48edea26d570fba53e4599ab7dcf01a74
Emil Abraham <emilabraham1414@gmail.com>
2021-12-12 09:06:17 -0500

    WIP: Processing input

--- day11/octopus.py
+++ day11/octopus.py
@@ -1,3 +1,29 @@
 # Simulate octopus flashes
 fp = open("input2")
 lines = fp.readlines()
+grid = []
+
+# Represents a point with an x and y coordinate.
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def printMe(self):
+        return (str(self.x) + "," + str(self.y))
+
+# Remove newline character from a string
+def removeNewLine(s):
+    return s.replace('\n', '')
+
+# Create the grid from the input
+def createGrid(grid, lines):
+    grid = []
+    for line in lines:
+        line = removeNewLine(line)
+        line = [x for x in line]
+        grid.append(list(map(int, line)))
+
+createGrid(grid, lines)
+print(lines)
+print(grid)
