5fe4e8b75d8f21f372f92929ae03f48c4a70f641
Emil Abraham <emilabraham1414@gmail.com>
2021-12-16 20:47:44 -0500

    Day 13 Part 1 and 2

--- day13/origami.py
+++ day13/origami.py
@@ -1,5 +1,5 @@
 # Fold the transparent paper and align the dots
-fp = open("input2")
+fp = open("input")
 lines = fp.readlines()
 coordinates = []
 folds = []
@@ -12,6 +12,15 @@ class Point:
 
     def printMe(self):
         return (str(self.x) + "," + str(self.y))# Remove newline character from a string
+
+# Represents a fold
+class Fold:
+    def __init__(self, axis, direction):
+        self.axis = axis
+        self.direction = direction
+
+    def printMe(self):
+        return (str(self.axis) + "," + str(self.direction))
 
 def removeNewLine(s):
     return s.replace('\n', '')
@@ -36,6 +45,16 @@ def convertToPoints(coordinates):
         points.append(Point(x, y))
     return points
 
+# Convert the input into folds
+def convertToFolds(folds):
+    newFolds = []
+    for fold in folds:
+        fold = fold.split(" ")[2]
+        fold = fold.split("=")
+        fold = Fold(int(fold[1]), fold[0])
+        newFolds.append(fold)
+    return newFolds
+
 # Find the largest x and y coordinate
 def findLargestValue(points):
     largestValues= [0, 0]
@@ -57,13 +76,64 @@ def createGrid(points, largestValues):
         grid[point.y][point.x] = "#"
     return grid
 
+# Print the grid nicely
 def printGrid(grid):
     for row in grid:
         for column in row:
             print(column, end="")
         print()
 
+# Perform a horizontal fold along axis
+def performYFold(grid, axis):
+    index = 1
+    for y in range(axis + 1, len(grid)):
+        for x in range(0, len(grid[y])):
+            if grid[y][x] == "#":
+                grid[axis-index][x] = "#"
+        index += 1
+
+    grid = grid[:axis]
+    return grid
+
+# Perform a horizontal fold along axis
+def performXFold(grid, axis):
+    index = 1
+    for y in range(0, len(grid)):
+        for x in range(axis + 1, len(grid[y])):
+            if grid[y][x] == "#":
+                grid[y][axis-index] = "#"
+            index += 1
+        index = 1
+
+    for (i, row) in enumerate(grid):
+        grid[i] = row[:axis]
+
+    return grid
+
+# Count the number of dots in the grid
+def countDots(grid):
+    count = 0
+    for row in grid:
+        for column in row:
+            if column == "#":
+                count += 1
+    print(count)
+
+# Peform the folds on the grid
+def performFolds(grid, folds):
+    for fold in folds:
+        if fold.direction =="y":
+            grid = performYFold(grid, fold.axis)
+        if fold.direction =="x":
+            grid = performXFold(grid, fold.axis)
+
+    return grid
+
 coordinates = convertToPoints(coordinates)
 largetsValues = findLargestValue(coordinates)
 grid = createGrid(coordinates, largetsValues)
-printGrid(grid)
+# grid = performYFold(grid, 7)
+grid = performXFold(grid, 655)
+countDots(grid)
+folds = convertToFolds(folds)
+printGrid(performFolds(grid, folds))
