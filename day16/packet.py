37aa79a47b25140e3662c53ad6b64c0336c70ffb
Emil Abraham <emilabraham1414@gmail.com>
2021-12-18 18:25:15 -0500

    Day 16 Still stuck

--- day16/packet.py
+++ day16/packet.py
@@ -13,26 +13,37 @@ def hexStringToBinary(hexString):
         binary += hexToBinary(hexString[i])
     return binary
 
-binaryString = hexStringToBinary("38006F45291200")
+binaryString = hexStringToBinary("C0015000016115A2E0802F182340")
 
 # Convert a binary number to decimal
 def binaryToDecimal(binary):
     return int(binary, 2)
 
+def breakOutEarly(binaryString, index, increment):
+    if index + increment >= len(binaryString):
+        return True
+    return False
+
 # find the packet version
-def findPacketVersion(binaryString, index):
+def findPacketVersion(binaryString, index, versionSum):
+    if breakOutEarly(binaryString, index, 3):
+        return versionSum, -1
     version = binaryToDecimal(binaryString[index:index+3])
     index += 3
     return version, index
     
 # find the packet type
 def findPacketType(binaryString, index):
+    if breakOutEarly(binaryString, index, 3):
+        return 0, -1
     type =  binaryToDecimal(binaryString[index:index+3])
     index += 3
     return type, index
 
 # Keep reading bits of 5 as long as the first bit is 1
 def findLiteralValue(binaryString, index):
+    if breakOutEarly(binaryString, index, 5):
+        return 0, -1
     literalValue = ""
     keepReading = True
     while keepReading:
@@ -45,48 +56,69 @@ def findLiteralValue(binaryString, index):
 
 # Find the length type Id
 def findPacketLengthTypeId(binaryString, index):
+    if breakOutEarly(binaryString, index, 1):
+        return 0, -1
     lengthType =  binaryString[index:index+1]
     index += 1
     return lengthType, index
 
 def findPacketCount(binaryString, index):
+    if breakOutEarly(binaryString, index, 11):
+        return 0, -1
     packetCount = binaryToDecimal(binaryString[index:index+11])
     index += 11
     return packetCount, index
 
 # Find the length of the packet 
 def findSubPacketLength(binaryString, index):
+    if breakOutEarly(binaryString, index, 15):
+        return None, -1
     length = binaryToDecimal(binaryString[index:index+15])
     index += 15
     return length, index
 
+
+
 # Recursively add version of each packet
 def addVersions(binaryString, versionSum, index, packetCount, bitCount):
     originalIndex = index
-    version, index = findPacketVersion(binaryString, index)
+    version, index = findPacketVersion(binaryString, index, versionSum)
+    if index < 0:
+        return version + versionSum
     versionSum += version
     type, index = findPacketType(binaryString, index)
+    if index < 0:
+        return versionSum
     if type == 4:
         literalValue, index = findLiteralValue(binaryString, index)
+        if index < 0:
+            return versionSum
     else:
         lengthTypeId, index = findPacketLengthTypeId(binaryString, index)
+        if index < 0:
+            return versionSum
         if lengthTypeId == "0":
             bitCount, index = findSubPacketLength(binaryString, index)
-            versionSum += addVersions(binaryString, versionSum, index, packetCount, bitCount)
-            bitCount = 0
+            if index < 0:
+                return versionSum
+            breakpoint()
+            addVersions(binaryString, versionSum, index, packetCount, bitCount)
         else:
             packetCount, index = findPacketCount(binaryString, index)
-            versionSum += addVersions(binaryString, versionSum, index, packetCount, bitCount)
-            packetCount = 0
+            if index < 0:
+                return versionSum
+            addVersions(binaryString, versionSum, index, packetCount, bitCount)
 
-    if bitCount != 0:
-        print(bitCount)
+    if packetCount > 0:
+        packetCount -= 1
+        return addVersions(binaryString, versionSum, index, packetCount, bitCount)
+    if bitCount > 0:
         bitCount -= (index - originalIndex)
         return addVersions(binaryString, versionSum, index, packetCount, bitCount)
-    elif packetCount != 0:
-        return addVersions(binaryString, versionSum, index, packetCount - 1, bitCount)
-    else:
-        breakpoint()
-        return versionSum
+
+    return versionSum
 
 print(addVersions(binaryString, 0, 0, 0, 0))
+# C0015000016115A2E0802F182340
+# This one doesn't work, while all the others do. I'm not sure why.
+# Are there any other places besides literal value where we remove leading zeroes?
