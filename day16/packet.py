113ea04b4a0d6e545eff122034683f748b6cf3d0
Emil Abraham <emilabraham1414@gmail.com>
2021-12-23 17:22:20 -0500

    WIP Continue Day 16 part 2

--- day16/packet.py
+++ day16/packet.py
@@ -13,37 +13,38 @@ def hexStringToBinary(hexString):
         binary += hexToBinary(hexString[i])
     return binary
 
-binaryString = hexStringToBinary("C200B40A82")
+binaryString = hexStringToBinary("9C0141080250320F1802104A08")
+print(binaryString)
 
 # Convert a binary number to decimal
 def binaryToDecimal(binary):
     return int(binary, 2)
 
-def breakOutEarly(binaryString, index, increment):
-    if index + increment >= len(binaryString):
+def breakOutEarly(binaryString, index, increment, accumulator):
+    if index + increment > len(binaryString):
         breakpoint()
         return True
     return False
 
 # find the packet version
-def findPacketVersion(binaryString, index):
-    if breakOutEarly(binaryString, index, 3):
+def findPacketVersion(binaryString, index, accumulator):
+    if breakOutEarly(binaryString, index, 3, accumulator):
         return 0, -1
     version = binaryToDecimal(binaryString[index:index+3])
     index += 3
     return version, index
     
 # find the packet type
-def findPacketType(binaryString, index):
-    if breakOutEarly(binaryString, index, 3):
+def findPacketType(binaryString, index, accumulator):
+    if breakOutEarly(binaryString, index, 3, accumulator):
         return 0, -1
     type =  binaryToDecimal(binaryString[index:index+3])
     index += 3
     return type, index
 
 # Keep reading bits of 5 as long as the first bit is 1
-def findLiteralValue(binaryString, index):
-    if breakOutEarly(binaryString, index, 5):
+def findLiteralValue(binaryString, index, accumulator):
+    if breakOutEarly(binaryString, index, 5, accumulator):
         return 0, -1
     literalValue = ""
     keepReading = True
@@ -56,23 +57,23 @@ def findLiteralValue(binaryString, index):
     return binaryToDecimal(literalValue), index
 
 # Find the length type Id
-def findPacketLengthTypeId(binaryString, index):
-    if breakOutEarly(binaryString, index, 1):
+def findPacketLengthTypeId(binaryString, index, accumulator):
+    if breakOutEarly(binaryString, index, 1, accumulator):
         return 0, -1
     lengthType =  binaryString[index:index+1]
     index += 1
     return lengthType, index
 
-def findPacketCount(binaryString, index):
-    if breakOutEarly(binaryString, index, 11):
+def findPacketCount(binaryString, index, accumulator):
+    if breakOutEarly(binaryString, index, 11, accumulator):
         return 0, -1
     packetCount = binaryToDecimal(binaryString[index:index+11])
     index += 11
     return packetCount, index
 
 # Find the length of the packet 
-def findSubPacketLength(binaryString, index):
-    if breakOutEarly(binaryString, index, 15):
+def findSubPacketLength(binaryString, index, accumulator):
+    if breakOutEarly(binaryString, index, 15, accumulator):
         return None, -1
     length = binaryToDecimal(binaryString[index:index+15])
     index += 15
@@ -84,38 +85,43 @@ def findSubPacketLength(binaryString, index):
 # I think I'm having trouble properly breaking out of this.
 # If I  put a breakpoint inside the breakOutEarly function, it ends up correct without looping.
 def addVersions(binaryString, accumulator, index, packetCount, bitCount, currentType, isSecond):
+    breakpoint()
     originalIndex = index
-    version, index = findPacketVersion(binaryString, index)
-    type, index = findPacketType(binaryString, index)
+    version, index = findPacketVersion(binaryString, index, accumulator)
+    type, index = findPacketType(binaryString, index, accumulator)
+    latestType = -1
     if type == 4:
-        literalValue, index = findLiteralValue(binaryString, index)
-        if currentType == -1 or currentType == 4:
-            return accumulator
-        elif currentType == 0:
-                accumulator += literalValue
-        elif currentType == 1:
-                accumulator *= literalValue
-        elif currentType == 2:
+        latestType = -1 if len(currentType) == 0 else currentType.pop()
+        literalValue, index = findLiteralValue(binaryString, index, accumulator)
+        if latestType == -1 or latestType == 4:
+            accumulator = literalValue
+        elif latestType == 0:
+            accumulator += literalValue
+        elif latestType == 1:
+            if accumulator == 0:
+                accumulator = 1
+            accumulator *= literalValue
+        elif latestType == 2:
             if accumulator == 0 or literalValue <= accumulator:
                 accumulator = literalValue
-        elif currentType == 3:
+        elif latestType == 3:
             if accumulator == 0 or literalValue >= accumulator:
                 accumulator = literalValue
-        elif currentType == 5:
+        elif latestType == 5:
             if isSecond:
                 accumulator = (1 if accumulator > literalValue else 0)
                 isSecond = False
             else:
                 accumulator = literalValue
                 isSecond = True
-        elif currentType == 6:
+        elif latestType == 6:
             if isSecond:
                 accumulator = (1 if accumulator < literalValue else 0)
                 isSecond = False
             else:
                 accumulator = literalValue
                 isSecond = True
-        elif currentType == 7:
+        elif latestType == 7:
             if isSecond:
                 accumulator = (1 if accumulator == literalValue else 0)
                 isSecond = False
@@ -123,25 +129,30 @@ def addVersions(binaryString, accumulator, index, packetCount, bitCount, current
                 accumulator = literalValue
                 isSecond = True
     else:
-        lengthTypeId, index = findPacketLengthTypeId(binaryString, index)
+        currentType.append(type)
+        lengthTypeId, index = findPacketLengthTypeId(binaryString, index, accumulator)
         if lengthTypeId == "0":
-            newBitCount, index = findSubPacketLength(binaryString, index)
+            newBitCount, index = findSubPacketLength(binaryString, index, accumulator)
             bitCount += newBitCount
-            addVersions(binaryString, accumulator, index, packetCount, bitCount, type, isSecond)
+            addVersions(binaryString, accumulator, index, packetCount, bitCount, currentType, isSecond)
             bitCount = 0
         else:
-            newPacketCount, index = findPacketCount(binaryString, index)
+            newPacketCount, index = findPacketCount(binaryString, index, accumulator)
             packetCount += newPacketCount
-            addVersions(binaryString, accumulator, index, packetCount, bitCount, type, isSecond)
+            addVersions(binaryString, accumulator, index, packetCount, bitCount, currentType, isSecond)
             packetCount = 0
 
     if packetCount > 0:
         packetCount -= 1
-        return addVersions(binaryString, accumulator, index, packetCount, bitCount, type, isSecond)
+        if (packetCount != 0 and latestType is not -1):
+            currentType.append(latestType)
+        return addVersions(binaryString, accumulator, index, packetCount, bitCount, currentType, isSecond)
     if bitCount > 0:
         bitCount -= (index - originalIndex)
-        return addVersions(binaryString, accumulator, index, packetCount, bitCount, type, isSecond)
+        if (bitCount != 0 and latestType is not -1):
+            currentType.append(latestType)
+        return addVersions(binaryString, accumulator, index, packetCount, bitCount, currentType, isSecond)
 
     return accumulator
 
-print(addVersions(binaryString, 0, 0, 0, 0, -1, False))
+print(addVersions(binaryString, 0, 0, 0, 0, [], False))
